55dc6ef95ee246898016e71904aa007c7a0cf240
yashas <yashasgc27@gmail.com>
2020-10-05 20:04:38 +0530

    done with routes

---
+++ controllers/events.js
@@ -0,0 +1,11 @@
+exports.postevent = (req,res,next) => {
+    
+}
+
+exports.getevent = (req,res,next) => {
+    res.render('eventregister');
+}
+
+exports.getHome = (req,res,next) => {
+    res.render('events');
+}
--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
+const eventController = require('../controllers/events');
 
+router.get('/eventregister',eventController.getevent);
 
-router.get('/eventregister',(req,res,next) => {
-    res.render('eventregister');
-})
-router.get('/',(req,res,next) => {
-    res.render('events');
-})
+router.post('/eventregister',eventController.postevent);
+
+router.get('/',eventController.getHome);
 
 module.exports = router;
