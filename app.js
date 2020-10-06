c638b002512fb0be8da3cf0b4773d8e5b6f9c77a
yashas <yashasgc27@gmail.com>
2020-10-06 16:24:47 +0530

    removed custom port from environment variable

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(eventRoutes);
 
 
 const MONGODB_URI = process.env.MONGODB_URI;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 db.connect(MONGODB_URI)
 .then(res => {
     console.log('database connected');
